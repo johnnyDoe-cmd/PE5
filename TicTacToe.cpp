a4894aa1e551c19c1da288f81aa0ed50c8d98111
johnnyDoe-cmd <trco9595@colorado.edu>
2020-02-13 19:30:26 -0700

    added display function

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -1,6 +1,14 @@
 int** CreateBoard(){
 	return int x[3][3];
 }
+void DisplayBoard(int** board){
+	for(int i = 1; i< 4; i ++){
+		for(int j = 1; j<4; j++){
+			cout<<board[i][j];
+		}
+	}
+	cout<<endl;
+}
 int main(){
 
 	
