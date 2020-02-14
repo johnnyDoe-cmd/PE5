2bf72ece86f664ea9ef173221b567725d2d72b71
johnnyDoe-cmd <61035223+johnnyDoe-cmd@users.noreply.github.com>
2020-02-14 12:21:17 -0700

    added GetPlayerChoice and PlaceMarker/fixed bugs

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -5,18 +5,29 @@ void DisplayBoard(int** tmp){
 	for(int i = 0; i < 3; i++){
 		for(int j = 0; j < 3; j++){
 			if(tmp[i][j] == 0){
-				cout<<"O"
+				cout<<"O";
 			}
 			else if(tmp[i][j] == 1){
-				cout<<"X"
+				cout<<"X";
 			}
 			else{
-				cout<<" "
+				cout<<" ";
 			}
 		}
 		cout<<endl;
 	}
 
+}
+int* GetPlayerChoice(){
+	int ans = [2];
+	cout<<"Please type an x coord (0,3)"<<endl;
+	cin>>ans[0];
+	cout<<"Please type a y coord (0,3)"<<endl;
+	cin>>ans[1];
+	return ans;
+}
+int** PlaceMarker(int tmp**,int x,int y,int type){
+	tmp[x][y] = type;
 }
 int main(){
 
