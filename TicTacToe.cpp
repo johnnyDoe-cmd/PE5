498b310427c261787224399ff2a004a1ffcfa3a4
johnnyDoe-cmd <61035223+johnnyDoe-cmd@users.noreply.github.com>
2020-02-14 12:10:01 -0700

    Update TicTacToe.cpp

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -1,13 +1,22 @@
 int** CreateBoard(){
 	return int x[3][3];
 }
-void DisplayBoard(int** board){
-	for(int i = 1; i< 4; i ++){
-		for(int j = 1; j<4; j++){
-			cout<<board[i][j];
+void DisplayBoard(int** tmp){
+	for(int i = 0; i < 3; i++){
+		for(int j = 0; j < 3; j++){
+			if(tmp[i][j] == 0){
+				cout<<"O"
+			}
+			else if(tmp[i][j] == 1){
+				cout<<"X"
+			}
+			else{
+				cout<<" "
+			}
 		}
+		cout<<endl;
 	}
-	cout<<endl; 
+
 }
 int main(){
 
