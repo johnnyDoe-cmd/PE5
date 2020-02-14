7c94cab0f1d625f4f0ef295cd32ca4a0342abdf3
johnnyDoe-cmd <61035223+johnnyDoe-cmd@users.noreply.github.com>
2020-02-14 13:47:18 -0700

    added meaningful comments

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -4,12 +4,15 @@ using namespace std;
 // Name:Travis Cochran
 // Assignment PE4p2
 
+//creates 2-d vector of all 2's
+//2 is the defualt empty cell
 vector<vector<int> > CreateBoard(){
 	vector<vector<int> > x{ { 2, 2, 2 }, 
                                { 2, 2, 2 }, 
                                { 2, 2, 2 } }; 
 	return x;
 }
+//prints characters based on int values in board
 void DisplayBoard(vector<vector<int> > tmp){
 	for(int i =0; i < 3; i++){
 		for(int j = 0; j < 3; j++){
@@ -20,6 +23,7 @@ void DisplayBoard(vector<vector<int> > tmp){
 				cout<<"X";
 			}
 			else{
+				//E is for empty
 				cout<<"E";
 			}
 		}
@@ -27,8 +31,10 @@ void DisplayBoard(vector<vector<int> > tmp){
 	}
 
 }
+//gets input from one player for thier choice
 vector<int> GetPlayerChoice(){
 	int tmp;
+	//ans holds choseen coords
 	vector<int> ans;
 	cout<<"Please type an x coord (0,2)"<<endl;
 	cin>>tmp;
@@ -38,23 +44,30 @@ vector<int> GetPlayerChoice(){
 	ans.push_back(tmp);
 	return ans;
 }
+//returns new copy of 2-d vector wiith updated cell
 vector<vector<int> > PlaceMarker(vector<vector<int> > tmp,int x,int y,int type){
 	tmp[x][y] = type;
 	return tmp;
 }
 int main(){
-	//used to change player symbol
+	//new board
 	vector<vector<int> > board = CreateBoard();
+	//vectors to hold player choices
 	vector<int> choice1;
 	vector<int> choice2;
+	//used to change player symbol
 	int counter = 1;
+	//endless while loop
 	bool done = false;
 	while(done != true){
 		//player1
 		cout<<"Player 1's turn:"<<endl;
+		//get choice coords
 		choice1 = GetPlayerChoice();
 		board = PlaceMarker(board,choice1[1],choice1[0],counter%2);
+		//show baord for nxt player
 		DisplayBoard(board);
+		//chnage symbol
 		counter++;
 		//player2
 		cout<<"Player 2's turn:"<<endl;
